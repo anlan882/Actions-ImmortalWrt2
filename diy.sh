095381e1f509adfa0283850711b023cb8ed35add
anlan882 <71642681+anlan882@users.noreply.github.com>
2026-08-20 22:05:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,10 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=128/' .config
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=2048/' .config
+echo "CONFIG_PACKAGE_luci-app-passwall=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-store=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-eqos=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
